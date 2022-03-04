9bea75dff52e6f8dfe893e88c0548a9b3c168a3b
yu hsuan <yvonne20030929@gmail.com>
2022-03-04 15:30:34 +0800

    Hello

--- src/main/java/com/yvonne/Hello.java
+++ src/main/java/com/yvonne/Hello.java
@@ -2,13 +2,6 @@ package com.yvonne;
 
 public class Hello {
     public static void main(String[] args) {
-        String[] names = {"yvonne","emily","mary","kris"};
-        int[] english = {80,70,60,50};
-        int[] math = {76,87,69,90};
-        for (int i = 0; i < 4; i++) {
-            if (i!=3){
-                System.out.println(names[i]+"\t"+english[i]+"\t"+math[i]+"\t"+(english[i]+math[i])/2);
-            }
-        }
+        System.out.println("hello world");
     }
 }
