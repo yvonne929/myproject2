19e4bdea9477a95bff677e8a9c84ec15efd4e4ef
yvonne929 <yvonne20030929@gmail.com>
2022-03-18 16:24:07 +0800

    Customer4

--- src/main/java/com/yvonne/GoldenCustomer.java
+++ src/main/java/com/yvonne/GoldenCustomer.java
@@ -4,5 +4,6 @@ public class GoldenCustomer extends SilverCustomer{
     public GoldenCustomer(String number, int prices){
     super(number,prices);
     off = 0.2f;
+    //和SilverCustomer一樣會印出回饋金等
     }
 }
