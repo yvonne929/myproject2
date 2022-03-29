52e55b34e3b7c965f5a517a7e890673e13fa8694
yu hsuan <yvonne20030929@gmail.com>
2022-03-29 14:09:53 +0800

    Station

---
+++ src/main/java/com/yvonne/buyTicket0325/Station.java
@@ -0,0 +1,7 @@
+package com.yvonne.buyTicket0325;
+
+public class Station {
+    public static final Station TAIPEI_STATION = new Station(100,"Taipei");
+    public static final Station TAICHUNG_STATION = new Station(200,"Taichung");
+    public static final Station KAOHSIUNG_STATION = new Station(300,"kaohsiung");
+}
