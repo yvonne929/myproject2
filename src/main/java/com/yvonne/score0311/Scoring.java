13a4fca2dcf5d7eaa0d4ca1fb4ede8471be32b48
yu hsuan <yvonne20030929@gmail.com>
2022-04-02 23:53:18 +0800

    Scoring1

--- src/main/java/com/yvonne/score0311/Scoring.java
+++ src/main/java/com/yvonne/score0311/Scoring.java
@@ -1,12 +1,23 @@
 package com.yvonne.score0311;
 
+import java.util.ArrayList;
+
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student("Jack", 78, 62);
+        ArrayList<Student> score = new ArrayList<Student>();
+
+        /*Student jack = new Student("Jack", 78, 62);
         Student hank = new Student("Hank", 60, 30);
         GraduateStudent jane = new GraduateStudent("Jane", 65, 82, 79);
         jack.print();
         hank.print();
-        jane.print();
+        jane.print();*/
+
+        score.add(new Student("Jack", 78, 62));
+        score.add(new Student("Hank", 60, 30));
+        score.add(new GraduateStudent("Jane", 65, 82, 79));
+        for (int i = 0; i < 4; i++) {
+            score.get(i).print();
+        }
     }
 }
