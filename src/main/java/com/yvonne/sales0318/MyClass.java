5e18ccba78094c74a3c0465aba74f234ddc7b9e9
yu hsuan <yvonne20030929@gmail.com>
2022-05-06 14:07:47 +0800

    interface practice

---
+++ src/main/java/com/yvonne/sales0318/MyClass.java
@@ -0,0 +1,10 @@
+package com.yvonne.sales0318;
+
+public class MyClass implements MyInterface{
+    public void test(){
+
+    }
+    public int max(int a , int b){
+        return a;
+    }
+}
---
+++ src/main/java/com/yvonne/sales0318/MyInterface.java
@@ -0,0 +1,6 @@
+package com.yvonne.sales0318;
+
+public interface MyInterface {
+    public void test();
+    public int max(int a , int b);
+}
