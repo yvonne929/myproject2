9b0b7fe38a9971cd52b02be541ac28789049f9e7
yu hsuan <yvonne20030929@gmail.com>
2022-05-13 15:27:09 +0800

    SimpleDateFormat practice

--- src/main/java/com/yvonne/parking/Runner.java
+++ src/main/java/com/yvonne/parking/Runner.java
@@ -1,11 +1,16 @@
 package com.yvonne.parking;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Runner {
     public static void main(String[] args) {
-        //String id =
-        Date d1 = new Date();
+        String enterTime = "08:00";
+        String exitTime = "11:00";
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");//設定格式
+        Date d1 = sdf.parse(enterTime);//把字串轉為物件
+
+
         System.out.println(d1);//印出現在時間格式
         System.out.println(d1.getTime());//印出毫秒(從以前到現在的毫秒數)
         long ms = 3*60*60*1000; //3小時換算成毫秒(一分鐘 = 1000毫秒)
