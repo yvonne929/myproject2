d27192c68560a211308cc922e2798cd409516c8e
yu hsuan <yvonne20030929@gmail.com>
2022-05-13 16:02:54 +0800

    price

--- src/main/java/com/yvonne/parking/Runner.java
+++ src/main/java/com/yvonne/parking/Runner.java
@@ -8,8 +8,9 @@ public class Runner {
         String enterTime = "08:00";
         String exitTime = "11:00";
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");//設定格式
+        Date d1 = null;
         try {
-        Date d1 = sdf.parse(enterTime);//把字串轉為物件
+        d1 = sdf.parse(enterTime);//把字串轉為物件
         System.out.println(d1);//印出現在時間格式
         System.out.println(d1.getTime());//印出毫秒(從以前到現在的毫秒數)
         }
@@ -23,6 +24,8 @@ public class Runner {
             Date d2 = sdf.parse(exitTime);
             System.out.println(d2);
             System.out.println(d2.getTime());
+            int mins = (int)((d2.getTime()-d1.getTime()))/1000/60;//毫秒差值轉為分鐘
+            int fee = 30*(mins/60);//一小時收30元
         }
         catch (Exception e) {
             System.out.println("exit wrong format");
