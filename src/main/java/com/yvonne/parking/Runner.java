3bc3172ade5c0ca06246258aa0a0317a3fe706c9
yu hsuan <yvonne20030929@gmail.com>
2022-05-13 14:55:11 +0800

    set/get Date

--- src/main/java/com/yvonne/parking/Runner.java
+++ src/main/java/com/yvonne/parking/Runner.java
@@ -8,8 +8,9 @@ public class Runner {
         Date d1 = new Date();
         System.out.println(d1);//印出現在時間格式
         System.out.println(d1.getTime());//印出毫秒(從以前到現在的毫秒數)
-        long ms = 3*60*60*100; //一分鐘 = 100毫秒
+        long ms = 3*60*60*1000; //3小時換算成毫秒(一分鐘 = 1000毫秒)
         Date d2 = new Date();
+        d2.setTime(d1.getTime()+ms);//設定d2的時間，d1時間加3小時
         System.out.println(d2);
         System.out.println(d2.getTime());
     }
