515989157fc3fdbaeb00ba6a10eb281406fe2740
yu hsuan <yvonne20030929@gmail.com>
2022-05-20 14:42:11 +0800

    JFrame practice

---
+++ src/main/java/com/yvonne/ui/MyWindow.java
@@ -0,0 +1,14 @@
+package com.yvonne.ui;
+
+import javax.swing.*;
+
+public class MyWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(false);
+        //顯示介面
+        //()裡是boolean值，true會顯示介面，false則不會
+
+        System.out.println("END?");
+    }
+}
