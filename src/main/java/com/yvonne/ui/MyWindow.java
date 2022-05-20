ef170788eb00214594c90e1ebe524d2ab9b08c7c
yu hsuan <yvonne20030929@gmail.com>
2022-05-20 15:16:12 +0800

    set JFrame and add Button

--- src/main/java/com/yvonne/ui/MyWindow.java
+++ src/main/java/com/yvonne/ui/MyWindow.java
@@ -5,7 +5,13 @@ import javax.swing.*;
 public class MyWindow {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setVisible(false);
+        frame.setSize(600,400);//設定介面大小
+        frame.setLocation(300,200);//設定介面位置(原在左上角)
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//設定行為(關掉介面程式也會停止)
+        JButton button = new JButton("OK");//add component(元件)
+        frame.add(button);//在frame裡添加component
+
+        frame.setVisible(true);
         //顯示介面
         //()裡是boolean值，true會顯示介面，false則不會
 
