f12cdbbf63c706246929b751b0b0c4a1960144ed
yu hsuan <yvonne20030929@gmail.com>
2022-05-27 15:19:44 +0800

    label practice

--- src/main/java/com/yvonne/ui/GuessFrame.java
+++ src/main/java/com/yvonne/ui/GuessFrame.java
@@ -1,6 +1,9 @@
 package com.yvonne.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
     public GuessFrame(){
@@ -8,6 +11,20 @@ public class GuessFrame extends JFrame {
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        JButton button = new JButton("Hello");
+        JLabel label = new JLabel("world");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //System.out.println("Java");//顯示成文字(不在視窗內)
+                label.setText("Java");//會改變之前設定的label -> ("world")
+            }
+        });
+        setLayout(new FlowLayout());
+        add(button);
+        add(label);
+
         setVisible(true);
 
     }
