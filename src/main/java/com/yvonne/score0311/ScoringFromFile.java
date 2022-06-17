aca2efade3f1ab8b498d303476d4a449b784040e
yu hsuan <yvonne20030929@gmail.com>
2022-06-17 16:03:47 +0800

    讀取檔案、資料(exception)

--- src/main/java/com/yvonne/score0311/ScoringFromFile.java
+++ src/main/java/com/yvonne/score0311/ScoringFromFile.java
@@ -1,9 +1,25 @@
 package com.yvonne.score0311;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ScoringFromFile {
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader("scores.txt");
+        //檔案名稱可能打錯，會找不到檔案，所以要除錯
+        try{
+            FileReader fileReader = new FileReader("scores.txt");
+            int data = fileReader.read();
+            System.out.println(data);
+            data = fileReader.read();
+            System.out.println(data);
+        }
+        catch (FileNotFoundException exception){
+            System.out.println("檔案讀取失敗");
+        }
+        catch (IOException exception){
+            System.out.println("資料讀取失敗");
+        }
+        System.out.println("Testing");
     }
 }
