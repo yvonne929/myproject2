b645fd57f12eb894c163de54b7fa3d3765b1d679
yu hsuan <yvonne20030929@gmail.com>
2022-06-17 15:32:56 +0800

    scoring from file

---
+++ src/main/java/com/yvonne/score0311/ScoringFromFile.java
@@ -0,0 +1,9 @@
+package com.yvonne.score0311;
+
+import java.io.FileReader;
+
+public class ScoringFromFile {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader("scores.txt");
+    }
+}
